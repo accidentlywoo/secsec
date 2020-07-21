67a60373e386a2c4d2d7fc675e5504adc7a47939
accidentlywoo <accidentlywoo@gmail.com>
2020-07-21 15:43:01 +0900

    ServletContext.getRealPath()로 톰캣 배포 경로 파악하기!

--- back25/src/com/my/control/ModelServletTest.java
+++ back25/src/com/my/control/ModelServletTest.java
@@ -2,6 +2,7 @@ package com.my.control;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.my.dao.CustomerDAO;
 import com.my.dao.CustomerDAOOracle;
 import com.my.exception.FindException;
+import com.my.service.CustomerService;
 import com.my.vo.Customer;
 
 /**
@@ -20,14 +22,27 @@ public class ModelServletTest extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		CustomerDAO dao;
-		dao = new CustomerDAOOracle();
-		
+//		CustomerDAO dao;
+//		dao = new CustomerDAOOracle();
+//		
+//		try {
+//			Customer c = dao.selectById("id1");
+//			System.out.println(c);
+//		} catch (FindException e) {
+//			e.printStackTrace();
+//		}
+		ServletContext sc = this.getServletContext();
+		String realPath = sc.getRealPath("customers.properties");
+		System.out.println(realPath);
+		CustomerService service = new CustomerService(realPath);
 		try {
-			Customer c = dao.selectById("id1");
-			System.out.println(c);
+			Customer customer = service.findById("id1");
+			System.out.println(customer);
 		} catch (FindException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 }
