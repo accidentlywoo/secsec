0072a2df0f3f2efb24a9bf0e6e31e7351f2ff989
accidentlywoo <accidentlywoo@gmail.com>
2020-07-23 10:26:02 +0900

    servlet class별로 인스턴스 변수를 web.xml을 이용해 외부에서 설정하는 방법

--- back25/src/com/my/control/ParamServlet.java
+++ back25/src/com/my/control/ParamServlet.java
@@ -12,15 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ParamServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		ServletContext context = this.getServletContext();
-		String pm = context.getInitParameter("pm");
+		String pm = context.getInitParameter("pm"); // ServletContext parameter떙기기
 		String version = context.getInitParameter("version");
-		res.setContentType("text/html;charset=UTF-8");
+		
+		String charset = this.getInitParameter("charset"); // Servlet Class Parameter 땡겨오기
+		
+		String id = req.getParameter("id");
+		
+		res.setContentType("text/html;charset="+charset);
 		PrintWriter writer = res.getWriter();
 		writer.print("프로젝트 관리자 : "+pm);
-		writer.print("\n 프로젝트 버전 : " + version);
+		writer.print("<br>프로젝트 버전 : " + version);
+		writer.print("<br>사용자가 입력한 ID : " + id);
 	}
 }
