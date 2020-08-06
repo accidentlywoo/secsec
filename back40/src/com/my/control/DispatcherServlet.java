0e2188f617a1f1fcf0c941559a08812e3c82ee2c
accidentlywoo <accidentlywoo@gmail.com>
2020-08-06 09:30:49 +0900

    FrontController Pattern 사용해보기! servlet-name이 default,jsp인 정적 파일들을 먼저 거르고 url 패턴을 처리하는 방법은?

--- back40/src/com/my/control/DispatcherServlet.java
+++ back40/src/com/my/control/DispatcherServlet.java
@@ -1,5 +1,35 @@
 package com.my.control;
 
-public class DispatcherServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/") // .html, .jpg, .css등 static 파일 경로도 타게된다.
+public class DispatcherServlet extends HttpServlet{
+
+	private Controller loginController = new LoginController();
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("in DispatcherServlet");
+		String servletPath = request.getServletPath();
+		String forwardServletPath = loginController.excute(request, resp);
+		if("/login".equals(servletPath)) {
+			ServletContext sc = this.getServletContext();
+			String customerEnvFileName = this.getInitParameter("customerEnv");
+			if(!"".equals(forwardServletPath)) {
+				RequestDispatcher reqdp  =  request.getRequestDispatcher(servletPath);
+				reqdp.forward(request, resp);
+			}
+		}else if("/logout".equals(servletPath)) {
+			
+		}
+		
+	}
 }
